1da3ef3c0a83adee7895343a989162cb2ceb1605
karamakcsc <karamakcsc@hotmail.com>
2025-01-28 10:01:42 +0300

    location on sales track

--- masar_sales_track/masar_sales_track/doctype/shop_management/shop_management.js
+++ masar_sales_track/masar_sales_track/doctype/shop_management/shop_management.js
@@ -27,3 +27,32 @@ frappe.ui.form.on("Shop Management", {
 		});
 	},
 });
+navigator.geolocation.getCurrentPosition(function(position) {
+    frappe.ui.form.on('Shop Management', {
+        before_submit: function(frm) {
+                var longitude = position.coords.longitude;
+                var latitude = position.coords.latitude;
+				console.log(longitude);
+				console.log(latitude);
+                frappe.call({
+                    type: "GET",
+                    url: `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`,
+                    callback: function(r) {
+						console.log(r.address);
+                        frm.set_value('country', r.address.country || '');
+                        frm.set_value('city', r.address.city || '');
+                        frm.set_value('neighbourhood', r.address.neighbourhood || '');
+                        frm.set_value('road', r.address.road || '');
+                        frm.set_value('house_number', r.address.house_number || '');
+                        frm.set_value('full_address', r.display_name || '');
+                        frm.set_value('latitude', r.lat);
+                        frm.set_value('longitude', r.lon);
+                    },
+                    error: function(xhr, status, error) {
+                        console.log("Error fetching address from coordinates:", status, error);
+                    }
+                });
+            
+        }
+    });
+});
--- masar_sales_track/masar_sales_track/doctype/shop_management/shop_management.py
+++ masar_sales_track/masar_sales_track/doctype/shop_management/shop_management.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2024, KCSC and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from datetime import datetime
 
 class ShopManagement(Document):
-	pass
+	def on_submit(self):
+		self.add_date_time()
+  
+	def add_date_time(self):
+		self.date_time =  datetime.now()
